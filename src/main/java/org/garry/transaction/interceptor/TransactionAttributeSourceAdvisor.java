2db255b4512b5a32e447c1288079ba0948a35f41
garrychen <chenymj23@gmail.com>
2021-04-22 19:53:13 +0800

    Transaction Advisor

--- src/main/java/org/garry/transaction/interceptor/TransactionAttributeSourceAdvisor.java
+++ src/main/java/org/garry/transaction/interceptor/TransactionAttributeSourceAdvisor.java
@@ -1,13 +1,16 @@
 package org.garry.transaction.interceptor;
 
 import org.aopalliance.aop.Advice;
+import org.springframework.aop.ClassFilter;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.support.AbstractPointcutAdvisor;
 import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 /**
  * @ClassName TransactionAttributeSourceAdvisor
- * @Description TODO
+ * @Description Advisor driven by a {@link TransactionAttributeSource},used to include
+ *              a {@link TransactionInterceptor} only for methods that are transactional
  * @Author cy
  * @Date 2021/4/21 19:51
  */
@@ -16,14 +19,39 @@ public class TransactionAttributeSourceAdvisor extends AbstractPointcutAdvisor {
     @Nullable
     private TransactionInterceptor transactionInterceptor;
 
+    private final TransactionAttributeSourcePointcut pointcut = new TransactionAttributeSourcePointcut() {
+
+        @Override
+        @Nullable
+        protected TransactionAttributeSource getTransactionAttributeSource() {
+           return (transactionInterceptor != null ? transactionInterceptor.getTransactionAttributeSource() : null);
+        }
+    };
+
+    public TransactionAttributeSourceAdvisor() {
+    }
+
+    public TransactionAttributeSourceAdvisor(@Nullable TransactionInterceptor interceptor) {
+        setTransactionInterceptor(interceptor);
+    }
+
+    public void setTransactionInterceptor(@Nullable TransactionInterceptor interceptor) {
+        this.transactionInterceptor = interceptor;
+    }
+
+    public void setClassFilter(ClassFilter classFilter)
+    {
+        this.pointcut.setClassFilter(classFilter);
+    }
 
     @Override
     public Pointcut getPointcut() {
-        return null;
+        return this.pointcut;
     }
 
     @Override
     public Advice getAdvice() {
-        return null;
+        Assert.state(this.transactionInterceptor != null ,"No TransactionInterceptor set");
+        return this.transactionInterceptor;
     }
 }
--- src/main/java/org/garry/transaction/interceptor/TransactionAttributeSourcePointcut.java
+++ src/main/java/org/garry/transaction/interceptor/TransactionAttributeSourcePointcut.java
@@ -1,8 +1,10 @@
 package org.garry.transaction.interceptor;
 
 import org.springframework.aop.support.StaticMethodMatcherPointcut;
+import org.springframework.util.ObjectUtils;
 
 import java.io.Serializable;
+import java.lang.reflect.Method;
 
 /**
  * @ClassName TransactionAttributeSourcePointcut
@@ -12,4 +14,48 @@ import java.io.Serializable;
  * @Date 2021/4/21 19:59
  */
 abstract class TransactionAttributeSourcePointcut extends StaticMethodMatcherPointcut implements Serializable {
+
+    @Override
+    public boolean matches(Method method, Class<?> targetClass) {
+       if(targetClass != null && TransactionalProxy.class.isAssignableFrom(targetClass))
+       {
+           return false;
+       }
+
+        TransactionAttributeSource tas = getTransactionAttributeSource();
+       return (tas == null || tas.getTransactionAttribute(method,targetClass) != null);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if(this == other)
+        {
+            return true;
+        }
+
+        if(!(other instanceof TransactionAttributeSourcePointcut))
+        {
+            return false;
+        }
+
+        TransactionAttributeSourcePointcut otherPc = (TransactionAttributeSourcePointcut) other;
+        return ObjectUtils.nullSafeEquals(getTransactionAttributeSource(),otherPc.getTransactionAttributeSource());
+    }
+
+    @Override
+    public int hashCode() {
+       return TransactionAttributeSourcePointcut.class.hashCode();
+    }
+
+    @Override
+    public String toString() {
+       return getClass().getName() + ": " + getTransactionAttributeSource();
+    }
+
+    /**
+     * Obtain the underlying TransactionAttributeSource
+     * To be implemented by subclasses
+     * @return
+     */
+    protected abstract TransactionAttributeSource getTransactionAttributeSource();
 }
